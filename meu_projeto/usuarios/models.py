b82c3e3d1dac6af70935faceaad9efe45cf5b4b8
mateus barreto de souza <mateusbarreto@tecgraf.puc-rio.br>
2025-09-10 14:40:41 -0300

    Ajustando retorno da consulta do model Usuario

--- meu_projeto/usuarios/models.py
+++ meu_projeto/usuarios/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Usuario(AbstractUser):
     id = models.AutoField(primary_key=True)
-    is_admin = models.BooleanField(default=False, help_text="Designa se o usuário é um administrador", verbose_name="Administrador")
+    is_admin = models.BooleanField(default=True, help_text="Designa se o usuário é um administrador", verbose_name="Administrador")
     telefone = models.CharField(max_length=15, blank=True, null=True, verbose_name="Número de telefone")
     cpf = models.CharField(max_length=14, unique=True, blank=True, null=True, verbose_name="CPF")
     data_nascimento = models.DateField(blank=True, null=True, verbose_name="Data de nascimento")
@@ -15,4 +15,4 @@ class Usuario(AbstractUser):
     estado = models.CharField(max_length=50, blank=True, null=True, verbose_name="Estado")
 
     def __str__(self):
-        return f"{self.username} ({self.get_tipo_usuario_display()})"
+        return self.username

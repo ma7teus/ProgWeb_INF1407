16d1879c9ad0fc572090dba590bbd0f5dea5a598
ma7teus <mateusbarreto03@gmail.com>
2025-10-02 22:58:00 -0300

    Ajustando endereços de deploy

--- meu_projeto/meu_projeto/wsgi.py
+++ meu_projeto/meu_projeto/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meu_projeto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meu_projeto.meu_projeto.settings')
 
 application = get_wsgi_application()

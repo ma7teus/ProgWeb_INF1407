d8761eab5c878af1a00447f80ce6d2a4fd35ca6b
ma7teus <mateusbarreto03@gmail.com>
2025-10-02 23:10:44 -0300

    Ajustando build para criar superuser

--- build.sh
+++ build.sh
@@ -8,3 +8,13 @@ cd meu_projeto
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+    print('Superuser admin criado!')
+else:
+    print('Superuser admin já existe.')
+EOF
